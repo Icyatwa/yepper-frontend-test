c250c307cafbe26545a31c722e5d31a9ed9ee3ec
icyatwa <icyatwandoba@gmail.com>
2026-06-18 08:12:06 +0200

    changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { ArrowLeft, Eye, EyeOff } from 'lucide-react';
-import { Button, Input } from '../components/components';
 import { authAPI } from '../utils/api';
 
 const GoogleIcon = () => (
