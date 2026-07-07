7d2b20b953d0840671aade5597fecd5accf49f7c
icyatwa <icyatwandoba@gmail.com>
2026-07-07 16:58:30 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AuthProvider } from './context/AuthContext';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import Dost from './Dost';
-import ProtectedRoute from './components/ProtectedRoute';
+// import ProtectedRoute from './components/ProtectedRoute';
 
 // import TrafficGrant from './pages/TrafficGrant';
 
